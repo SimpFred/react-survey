b28f4d577a0491fb1d2544ccfad5198b78c65c54
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-09-26 15:10:10 +0200

    Refactor AnswersItem component and add prop type validation

--- src/components/AnswersItem.jsx
+++ src/components/AnswersItem.jsx
@@ -1,3 +1,5 @@
+import PropType from 'prop-types';
+
 // Components don't need to be separeted into individual files
 // Here we have a smaller component that helps compose the AnswersItem below
 
@@ -8,21 +10,26 @@ const answersSet = {
   noTime: "I don't like to spend time with it"
 };
 
-function ItemsList({ list }) {
+function ItemsList({ list}) {
   return (
-    <ul>
-      {list.map((item) => (
-        <li>{answersSet[item]}</li>
+    <>
+      {list.map((item, index) => (
+        console.log("item", item),
+        <li key={index}> {answersSet[item]}</li>
       ))}
-    </ul>
+      </>
   );
 }
+
+ItemsList.propTypes = {
+  list: PropType.array,
+};
 
 // This is the main component being exported from this file
 export default function AnswersItem({
   // Feel free to change this props names to what suits you best
   // Rememeber here we're destructuring answerItem, which is the prop name that we've passed
-  answerItem: { username, colour, timeSpent, review }
+  answerItem: { username, colour, timeSpent, review }, onEdit, index
 }) {
   return (
     <li>
@@ -32,15 +39,27 @@ export default function AnswersItem({
           <em>How do you rate your rubber duck colour?</em>
           <span className="answer__line">{colour}</span>
         </p>
-        <p>
           <em>How do you like to spend time with your rubber duck?</em>
-          <ItemsList list={timeSpent} />
-        </p>
+          <ul><ItemsList list={timeSpent} /></ul>
+          
         <p>
           <em>What else have you got to say about your rubber duck?</em>
           <span className="answer__line">{review}</span>
         </p>
+        <button className='answer__edit' onClick={() => onEdit(index)}>Edit</button>
       </article>
     </li>
   );
 }
+
+// This is a prop type validation for the component
+AnswersItem.propTypes = {
+  answerItem: PropType.shape({
+    username: PropType.string,
+    colour: PropType.string,
+    timeSpent: PropType.array,
+    review: PropType.string
+  }),
+  onEdit: PropType.func,
+  index: PropType.number
+};
